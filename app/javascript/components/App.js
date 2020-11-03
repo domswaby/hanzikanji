5b99eef1fc2e59b4933f5fd5ce9a3be2b1386a19
ds <ds@JZnati.local>
2020-11-04 00:01:17 +0800

    Add CangJie and Written_Zi images to homepage

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -9,13 +9,37 @@ import Home from "../pages/Home"
 import Reports from "../pages/Reports"
 import Products from "../pages/Products"
 import Contact from "../pages/Contact"
+import styled from "styled-components"
+import cangJie_img from "../../assets/images/CangjieBlackWhite.jpg";
+import charPic_img from "../../assets/images/Favicons/zi_written_small.jpg";
 import './App.css';
 
+const CangJie = styled.div`
+    position: absolute;
+    background: url(${cangJie_img});
+    background-repeat: no-repeat;
+    top: 0;
+    right: 80px;
+    height: 80px;
+    width: 87px;
+`;
+const CharPic = styled.div`
+    position: absolute;
+    background: url(${charPic_img});
+    background-size: contain;
+    background-repeat: no-repeat;
+    top: 0;
+    right: -7px;
+    height: 80px;
+    width: 87px;
+`;
 
 const App = () => {
   return (
     <Fragment>
       <Navbar />
+      <CangJie></CangJie>
+      <CharPic></CharPic>
       <Switch>
         <Route exact path="/kanjis/:page" component={Kanjis} />
         <Route exact path="/kanji/:num" component={Kanji} />
--- app/javascript/pages/Contact.js
+++ app/javascript/pages/Contact.js
@@ -5,13 +5,16 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  height: calc(100vh - 80px);
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
+  align-items: center
   width: 100%;
 `;
 const TwitterWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
   font-size: 2em;
   p {
@@ -19,8 +22,9 @@ const TwitterWrapper = styled.div`
   }
   div {
     text-align: center;
-    margin: 1em 0 0.5em 0;
+    margin: 0 0 0.5em 0;
     color: rgb(8, 160, 233);
+    font-size: 3em;
   }
   @media only screen and (max-width: 600px) {
     font-size: 1.5em;
@@ -30,13 +34,15 @@ const MailWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   font-size: 2em;
   p {
     text-align: center;
   }
   div {
     text-align: center;
-    margin: 2em 0 0.5em 0;
+    margin: 0 0 0.5em 0;
+    font-size: 3em;
   }
   @media only screen and (max-width: 600px) {
     font-size: 1.5em;
@@ -46,11 +52,11 @@ const MailWrapper = styled.div`
 function Contact() {
   return (
     <Wrapper>
-      <TwitterWrapper>
+      <TwitterWrapper href="https://twitter.com/HanziKanjiCards">
         <div>
           <FaTwitter />
         </div>
-        <p>Twitter</p>
+        <p><a href="https://twitter.com/HanziKanjiCards">@HanziKanjiCards</a></p>
       </TwitterWrapper>
 
       <MailWrapper>
