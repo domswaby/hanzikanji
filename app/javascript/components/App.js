e3a99ec98905bc24b02d18f9c7b3b37e3912c0c2
ds <domswaby@gmail.com>
2020-12-26 17:17:51 +0800

    make routes declarative by changing react-router v4 component prop style to react-router v5 child element style

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -5,33 +5,33 @@ import Airline from "./Airline/Airline";
 import Navbar from "./Navbar/Navbar";
 import Kanjis from "./Kanjis/Kanjis";
 import Kanji from "./Kanji/Kanji";
-import Home from "../pages/Home"
-import Reports from "../pages/Reports"
-import Products from "../pages/Products"
-import Contact from "../pages/Contact"
-import styled from "styled-components"
+import Home from "../pages/Home";
+import Reports from "../pages/Reports";
+import Products from "../pages/Products";
+import Contact from "../pages/Contact";
+import styled from "styled-components";
 import cangJie_img from "../../assets/images/CangjieBlackWhite.jpg";
 import charPic_img from "../../assets/images/Favicons/zi_written_small.jpg";
-import './App.css';
+import "./App.css";
 
 const CangJie = styled.div`
-    position: absolute;
-    background: url(${cangJie_img});
-    background-repeat: no-repeat;
-    top: 0;
-    right: 80px;
-    height: 80px;
-    width: 87px;
+  position: absolute;
+  background: url(${cangJie_img});
+  background-repeat: no-repeat;
+  top: 0;
+  right: 80px;
+  height: 80px;
+  width: 87px;
 `;
 const CharPic = styled.div`
-    position: absolute;
-    background: url(${charPic_img});
-    background-size: contain;
-    background-repeat: no-repeat;
-    top: 0;
-    right: 0;
-    height: 80px;
-    width: 87px;
+  position: absolute;
+  background: url(${charPic_img});
+  background-size: contain;
+  background-repeat: no-repeat;
+  top: 0;
+  right: 0;
+  height: 80px;
+  width: 87px;
 `;
 
 const App = () => {
@@ -41,11 +41,19 @@ const App = () => {
       <CangJie></CangJie>
       <CharPic></CharPic>
       <Switch>
-        <Route exact path="/list/:deck/:page" component={Kanjis} />
-        <Route exact path="/cards/:deck/:num" component={Kanji} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/" component={Home} />
+        <Route exact path="/list/:deck/:page"></Route>
+        <Route exact path="/cards/:deck/:num">
+          <Kanji />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/" >
+          <Home />
+        </Route>
         <Route exact path="/airlines/:slug" component={Airline} />
       </Switch>
     </Fragment>
--- app/javascript/components/Kanji/Kanji.js
+++ app/javascript/components/Kanji/Kanji.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import Card from "./Card";
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+
 
 const Home = styled.div``;
 const Controls = styled.div`
@@ -35,14 +37,17 @@ const Controls = styled.div`
 `;
 
 const Kanji = (props) => {
-  const num = props.match.params.num;
-  const deck_prop = props.match.params.deck;
+  const { num } = useParams();
+  const { deck } = useParams();
+  //const num = props.match.params.num;
+  //const deck = props.match.params.deck;
   // *** calculate the right pageNumber for this kanji, and the first and last kanji on that page ***
   let kanjisPerPage = 50;
   let pageNumber = Math.ceil(num / kanjisPerPage);
   let totalPages = 3030 / kanjisPerPage;
   let first_kanji_number = pageNumber * 50 - 49;
   let last_kanji_number = pageNumber * 50;
+
   let char_index = num - first_kanji_number;
 
   const [loaded, setLoaded] = useState(false);
@@ -53,36 +58,56 @@ const Kanji = (props) => {
   const [page, setPage] = useState(pageNumber);
 
   useEffect(() => {
-    // Get all of our kanjis from api
-    // Update kanjis in our state
+    // Get all kanjis from api
+    // Update kanjis in state
     axios
-      .get(`/api/v1/${props.match.params.deck}/page/${page}.json`)
+      .get(`/api/v1/${deck}/page/${page}.json`)
       .then((resp) => {
         setKanjis(resp.data.data);
+        getIndex(resp.data.data);
         setLoaded(true);
       })
       .catch((resp) => console.log(resp));
-  }, [ props.match.params.deck]);
+  }, []);
+
+  // find index of character received from params in kanjis
+
+  const getIndex = (input) => {
+    const data_for_get_index = input.map((item) => {
+      return item.attributes;
+    });
+    console.log("it worked");
+    console.log(data_for_get_index.length);
+
+    let idx1 = 0;
+    let cur_char = "";
+    // let char_to_find = props.match.params.char;
+    // for(idx1 = 0; idx1 < data_for_get_index.length; idx1++){
+    //   cur_char = data_for_get_index[idx1].char;
+    //   if(char_to_find = cur_char){
+    //     setIndex(idx1);
+    //   }
+    // }
+  };
 
   // *** find the index of the kanji received through params
   const getKanjis = (direction, new_page) => {
-      setLoaded(false);
+    setLoaded(false);
     axios
-      .get(`/api/v1/${props.match.params.deck}/page/${new_page}.json`)
+      .get(`/api/v1/${deck}/page/${new_page}.json`)
       .then((resp) => {
         if (direction === "up") {
           console.log("Im in the up direction if statement");
           console.log(resp.data.data);
           setKanjis(resp.data.data);
           setIndex(0);
-          setPage(old_page => old_page + 1);
-
+          setPage((old_page) => old_page + 1);
         } else {
           setKanjis(resp.data.data);
           setIndex(resp.data.data.length - 1);
-          setPage(old_page => old_page - 1);
+          setPage((old_page) => old_page - 1);
         }
-          setLoaded(true);
+        setLoaded(true);
       })
       .catch((resp) => console.log(resp));
   };
@@ -93,12 +118,11 @@ const Kanji = (props) => {
 
   const nextCard = () => {
     setShowInfo(false);
-    if (props.match.params.deck === "kanjis") {
+    if (deck === "kanjis") {
       if (data[index].number === 3030) {
       } else if (index === 49) {
         let new_page = page + 1;
         getKanjis("up", new_page);
-
       } else {
         let new_index = index + 1;
         setIndex(new_index);
@@ -121,12 +145,12 @@ const Kanji = (props) => {
       let new_page = page - 1;
       getKanjis("down", new_page);
     } else {
-      setIndex(old_index => old_index - 1);
+      setIndex((old_index) => old_index - 1);
     }
   };
 
   const toggleInfo = () => {
-    setShowInfo(old_show_info => !old_show_info);
+    setShowInfo((old_show_info) => !old_show_info);
   };
 
   return (
@@ -135,7 +159,7 @@ const Kanji = (props) => {
         <div>
           <Card
             data={data}
-            deck={props.match.params.deck}
+            deck={deck}
             index={index}
             pageNumber={page}
             toggle={toggleInfo}
