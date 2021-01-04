5670b9bfd0f6510b40e11c3397abb85d2fa53429
ds <domswaby@gmail.com>
2021-01-04 21:16:52 +0800

    remove build scripts and add stylesheet_pack_tag to application.html.erb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,11 +27,11 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #I changed this to true
-  config.assets.compile = true
+  config.assets.compile = false
   #added these ones myslef
-config.serve_static_assets = true
-config.assets.compile = true
-config.assets.digest = true
+# config.serve_static_assets = true
+# config.assets.compile = true
+# config.assets.digest = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
