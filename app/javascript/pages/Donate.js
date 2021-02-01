465f87b5a5d35211fed394cf04662a9dfa946afc
ds <domswaby@gmail.com>
2021-02-01 22:05:01 +0800

    add copy to clipboard success transition notification to donate component

--- app/javascript/pages/Donate.js
+++ app/javascript/pages/Donate.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { FaBitcoin } from "react-icons/fa";
 import { FaEthereum } from "react-icons/fa";
 import { AiTwotoneMail } from "react-icons/ai";
+import { AiOutlineCopy } from "react-icons/ai";
 import donate_btc_img from "../../assets/images/donate_btc.png";
 import styled from "styled-components";
 import Footer from "./Footer";
 
+
+
 const Wrapper = styled.div`
   display: flex;
+  position: relative;
   height: calc(100vh - 80px);
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center
   width: 100%;
+  .showSuccess{
+    top: 0px !important;
+    border-radius: 0 0 50% 50%;
+  }
+  .hideSuccess{
+    top: -80px !important;
+    border-radius: 0 0 0 0;
+  }
+  .hideSuccess, .showSuccess{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    p{
+      text-align: center;
+    }
+  }
 `;
 const TwitterWrapper = styled.div`
   display: flex;
+
   flex-direction: column;
   align-items: center;
   justify-content: center;
@@ -34,10 +57,10 @@ const TwitterWrapper = styled.div`
     background: url(${donate_btc_img}) !important;
     height: 125px;
     width: 125px;
-
+    margin: 0 0 0.5em 0;
   }
   .btc_alpha_num{
-    font-size: 0.5em; 
+    font-size: 0.5em;
   }
   div {
     text-align: center;
@@ -46,16 +69,44 @@ const TwitterWrapper = styled.div`
     font-size: 3em;
 
   }
+  AiOutlineCopy{
+    cursor: pointer;
+  }
+  .copy_icon{
+    cursor: pointer;
+  }
   @media only screen and (max-width: 600px) {
     font-size: 1.5em;
   }
 `;
+const CopySuccess = styled.div`
+  position: absolute;
+  height: 80px;
+  transition: 850ms;
+  background-color: rgb(141, 195, 81);
+  width: 100%;
+
+  z-index: -50;
+  font-size: 1.5em;
+  color: var(--dark-gray);
+
+`;
 
 
-function Donate() {
+const Donate = () => {
+
+  const [showCopy, setShowCopy] = useState(false);
+
+  const copyToClipboard = () => {
+    window.navigator.clipboard.writeText("1K3G2Cr7rbMmRm4jZNzdHcfTL1x1kGL69W");
+    setShowCopy(true);
+    setTimeout(function(){ setShowCopy(false); }, 3000);
+  };
+
   return (
     <div>
       <Wrapper>
+        <CopySuccess className={showCopy? "showSuccess" : "hideSuccess"}><p>Copied address to clipboard</p></CopySuccess>
         <TwitterWrapper href="https://twitter.com/HanziKanjiCards" target="_blank">
           <div>
           <h2>Bitcoin</h2>
@@ -63,7 +114,7 @@ function Donate() {
           </div>
           <div className="donate_btc_qr"></div>
           <p className="btc_alpha_num">
-            1K3G2Cr7rbMmRm4jZNzdHcfTL1x1kGL69W
+            1K3G2Cr7rbMmRm4jZNzdHcfTL1x1kGL69W <AiOutlineCopy className="copy_icon" onClick={copyToClipboard}/>
           </p>
         </TwitterWrapper>
       </Wrapper>
