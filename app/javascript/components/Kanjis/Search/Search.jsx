cd3d5b7446bb8b83b7b406a8da5c7cdd648883c4
ds <domswaby@gmail.com>
2021-02-25 19:59:38 +0800

    add second search input to search component for story searching

--- app/javascript/components/Kanjis/Search/Search.jsx
+++ app/javascript/components/Kanjis/Search/Search.jsx
@@ -1,86 +1,101 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import * as BsIcons from "react-icons/bs";
-
 
 const SearchWrap = styled.div`
   width: 100%;
   background-color: var(--lighter-gray);
-  p{
+  p {
     margin: 0 auto;
     padding: 0.2em 1em;
     text-align: center;
     background-color: var(--strong-red-home);
   }
-  div{
-    width: 65%;
-    margin: 0 auto;
-    padding: 1em 0;
-    display: flex;
-    justify-content: space-around;
-    input{
-      width: 50%;
-      padding: 0.5em;
-      font-size: 1.2em;
-      border: 4px solid var(--light-gray);
-      &:hover{
-        border: 4px solid var(--lighter-gray);
 
-      }
-      &:focus{
-        border: 4px solid var(--dark-gray);
-        outline: none;
-      }
+`;
+const OuterInputWrap = styled.div`
+  width: 65%;
+  margin: 0 auto;
+  padding: 1em 0;
+  display: flex;
+  justify-content: space-around;
+  input {
+    display: block;
+    width: 100%;
+    margin-bottom: 0.3em;
+    padding: 0.5em;
+    font-size: 1.2em;
+    border: 4px solid var(--light-gray);
+    &:hover {
+      border: 4px solid var(--lighter-gray);
     }
-    label{
-      padding: 0.5em 0.6em;
-      display: block;
+    &:focus {
+      border: 4px solid var(--dark-gray);
+      outline: none;
     }
-    button{
-      padding: 0.5em;
-      font-size: 1.2em;
-      border: 10px solid var(--light-gray);
-      width: 30%;
-      border-radius: 10%;
-      &:hover{
-        cursor: pointer;
-        border: 10px solid var(--dark-gray);
-
-      }
-      &:focus{
-        outline: none;
-        border: 10px solid var(--lighter-gray);
-
-      }
+  }
+  label {
+    padding: 0.5em 0.6em;
+    display: block;
+  }
+  button {
+    padding: 0.5em;
+    font-size: 1.2em;
+    border: 10px solid var(--light-gray);
+    width: 30%;
+    border-radius: 10%;
+    &:hover {
+      cursor: pointer;
+      border: 10px solid var(--dark-gray);
+    }
+    &:focus {
+      outline: none;
+      border: 10px solid var(--lighter-gray);
     }
   }
   @media only screen and (max-width: 600px) {
-    div{
       width: 80%;
-    }
   }
-`
+`;
+const InnerInputWrap = styled.div`
+  width: 50%;
+`;
 
 const Search = (props) => {
   const searchRequest = () => {
     props.doSearch(props.search);
-  }
+  };
   const updateSearch = (e) => {
     props.setSearch(e.target.value);
-  }
+  };
 
   return (
     <SearchWrap>
-      <div>
-        <input type="text" id="searcher" placeholder="Char / Meaning / #" onChange={updateSearch} />
-        <button onClick={searchRequest}>Search <BsIcons.BsSearch/></button>
-      </div>
+      <OuterInputWrap>
+        <InnerInputWrap>
+          <input
+            type="text"
+            id="searcher"
+            placeholder="Char / Meaning / #"
+            onChange={updateSearch}
+          />
+          <input
+            type="text"
+            id="searcher"
+            placeholder="Story (words)"
+
+          />
+        </InnerInputWrap>
+        <button onClick={searchRequest}>
+          Search <BsIcons.BsSearch />
+        </button>
+      </OuterInputWrap>
       {Object.keys(props.searchErr).map((key) => {
-          return <p>{props.searchErr[key]}</p>
+        return <p>{props.searchErr[key]}</p>;
       })}
     </SearchWrap>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
